fff2f08fdf643eea8a4f9d3dc78800294bd3470b
VergaraC <49599106+VergaraC@users.noreply.github.com>
2019-09-18 07:26:59 -0300

    ww

---
+++ C/assets/js/categorias.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', function() {
+  let params = coDesExtract()
+  let value = params['key']
+  let db = coDesConnect('https://grupo-l-fafae.firebaseio.com/')
+
+  let strlink = "projeto.html?pai=" + value + "&key={{@key}}"
+  let link = document.querySelector('.sub_title')
+  console.log(link)
+  link.href = strlink
+
+
+
+  
+  db.download('/', function(data) {
+
+    context = data['portfolio'][value]
+    coDesReplace('.category-title',context)
+    coDesReplace('.projetos-menu',context)
+
+  })
+})
